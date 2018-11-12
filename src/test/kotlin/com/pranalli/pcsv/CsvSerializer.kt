6cdc044f4f75b0c62bd682f222d1d66ee9e07da0
Pasquale Ranalli <pranalli@gmail.com>
2018-11-11 20:57:32 -0500

    Added CsvSerializer

---
+++ src/test/kotlin/com/pranalli/pcsv/CsvSerializer.kt
@@ -0,0 +1,32 @@
+package com.pranalli.pcsv
+
+import com.pranalli.pcsv.data.CsvFile
+
+object CsvSerializer {
+
+    fun toCsv(file: CsvFile, params: CsvSerializerParams) : String {
+        val sb = StringBuilder()
+        file.getRows().forEachIndexed { rowIndex, row ->
+
+            row.getValues().forEachIndexed { valueIndex, value ->
+                sb.append(value.getSerializedValue())
+                if (valueIndex < row.getValues().size)
+                    sb.append(',')
+            }
+
+            if (rowIndex < file.getRows().size)
+                sb.nl()
+        }
+
+        return sb.toString()
+    }
+
+    private fun StringBuilder.nl() {
+        this.append("\n\r")
+    }
+
+    class CsvSerializerParams {
+
+    }
+}
+
